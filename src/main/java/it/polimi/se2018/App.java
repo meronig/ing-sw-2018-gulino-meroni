8327ac30b0ad150bcd775cd7503d471cceeedc57
meronig <giovanni.meroni@polimi.it>
2018-04-10 17:39:50 +0200

    Commit iniziale

---
+++ src/main/java/it/polimi/se2018/App.java
@@ -0,0 +1,14 @@
+package it.polimi.se2018;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int unused;
+        System.out.println( "Hello World!" );
+    }
+}
