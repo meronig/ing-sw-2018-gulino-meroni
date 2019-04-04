9b595529462aa62b001c5fecf1ce1970fd847848
giovanni.meroni <giovanni.meroni@polimi.it>
2019-04-04 12:44:32 +0200

    Fixed, failing test

--- src/main/java/it/polimi/se2018/App.java
+++ src/main/java/it/polimi/se2018/App.java
@@ -10,6 +10,6 @@ public class App
     {
         int unused;
 
-        System.out.println( "Hello World! );
+        System.out.println( "Hello World!" );
     }
 }
--- src/test/java/it/polimi/se2018/AppTest.java
+++ src/test/java/it/polimi/se2018/AppTest.java
@@ -1,8 +1,8 @@
 package it.polimi.se2018;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -16,5 +16,9 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    public void shouldFail()
+    {
+        fail();
     }
 }
