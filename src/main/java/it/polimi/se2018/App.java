db7f69a751a0ec815f5f1afc0fba73d782c32b97
giovanni.meroni <giovanni.meroni@polimi.it>
2019-04-04 10:57:11 +0200

    intentionally broken

--- src/main/java/it/polimi/se2018/App.java
+++ src/main/java/it/polimi/se2018/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         int unused;
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Hello World! );
     }
 }
